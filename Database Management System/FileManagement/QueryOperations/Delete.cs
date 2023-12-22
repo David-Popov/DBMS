5e838665de90cff6dc0c319203d0304690ef2d6d
thevlad0 <the_vlad0@icloud.com>
2023-12-23 00:40:08 +0200

    Add Delete Query

--- Database Management System/FileManagement/QueryOperations/Delete.cs
+++ Database Management System/FileManagement/QueryOperations/Delete.cs
@@ -1,29 +1,47 @@
 ﻿using Database_Management_System.String;
 using Database_Management_System.LogicExpressionCalculator;
+using Database_Management_System.LogicExpressionCalculator.Expressions;
 using Database_Management_System.Validators.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Database_Management_System.DataStructures;
 
 namespace Database_Management_System.FileManagement.QueryOperations
 {
     public class Delete : Query
     {
-        private LogicExpressionCalculator.Expressions.Expression expr;
+        private string expr;
+        private string[] colNames;
 
         public Delete(string src) 
         {
             _tableName = StringFormatter.Substring(src, 0, ' ');
-            expr = ExpressionParser.ParseExpression(
-                   ExpressionParser.GetParsableExpression(
-                   StringFormatter.Substring(src, _tableName.Length + Queries.where.Length + 2)))!;
+            expr = ExpressionParser.GetParsableExpression(
+                   StringFormatter.Substring(src, _tableName.Length + Queries.where.Length + 2));
+            colNames = ExpressionParser.ExtractColumnNames(expr);
         }
 
         public override void execute()
         {
-            
+            DataArray data = new DataArray(_tableName);
+            int[] colIndexes = data.GetColumnIndexes(colNames);
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                string exprCopy = "";
+                for (int j = 0; j < colIndexes.Length; ++j)
+                    exprCopy = StringFormatter.ReplaceAll(expr, colNames[j], data[i][colIndexes[j]]);
+
+                Expression expression = ExpressionParser.ParseExpression(exprCopy)!;
+                if (expression.evaluate())
+                {
+                    data.DeleteRecord(i);
+                    --i;
+                }
+            }
         }
     }
 }
--- Database Management System/Program.cs
+++ Database Management System/Program.cs
@@ -3,19 +3,11 @@ using Database_Management_System.DataStructures;
 using Database_Management_System.FileManagement;
 using Database_Management_System.FileManagement.QueryOperations;
 using Database_Management_System.Validators.Constants;
+using Database_Management_System.LogicExpressionCalculator.Expressions;
+using Database_Management_System.LogicExpressionCalculator;
 
+/*var q = QueryParser.CreateQuery("Delete FROM Sample WHERE Id <> 2");
+q.execute();*/
 
-//var createQuery = QueryParser.CreateQuery("CreateTable Sample(Id:int, Name:string, BirthDate:date default \"01.01.2022\")");
-
-//createQuery.execute();
-
-//var query = QueryParser.CreateQuery("Insert INTO Sample (Id, Name) VALUES (1, \"Pedro\")");
-
-//query.execute();
-
-var data = new DataArray("Sample", new FileStream(@$"{Utility.filesFolderPath}Sample.bin", FileMode.Open, FileAccess.ReadWrite));
-
+DataArray data = new DataArray("Sample");
 data.Print();
-
-data.DeleteRecord(2);
-
